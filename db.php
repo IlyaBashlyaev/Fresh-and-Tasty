2ad38809117501111836a6e34e940bcc425a7e3a
Ilya Bashlyaev <ibashlyaev2000@gmail.com>
2022-08-08 13:38:43 +0200

    Commit

--- db.php
+++ db.php
@@ -1,21 +1,8 @@
 <?php
-    if ($_SERVER['HTTP_HOST'] == 'fresh-and-tasty.herokuapp.com') {
-        $cleardb_url = parse_url(getenv('CLEARDB_DATABASE_URL'));
-        $server = $cleardb_url['host'];
-        $username = $cleardb_url['user'];
-        $password = $cleardb_url['pass'];
-        $db = substr($cleardb_url['path'], 1);
+    if ($_SERVER['HTTP_HOST'] == 'fresh-and-tasty.herokuapp.com')
+        $connection = new mysqli('sql11.freemysqlhosting.net', 'sql11511502', '9PXMcq2Mnj', 'sql11511502');
 
-        $active_group = 'default';
-        $query_builder = TRUE;
-    }
+    else
+        $connection = new mysqli('127.0.0.1', 'Ilya Bashlyaev', '#vOV(0y2#vOV(0y2', 'restaurant');
 
-    else {
-        $server = 'localhost';
-        $username = 'Ilya Bashlyaev';
-        $password = '#vOV(0y2#vOV(0y2';
-        $db = 'restaurant';
-    }
-
-    $connection = new mysqli($server, $username, $password, $db);
 ?>
