4b20a81c08e5c6eeae72c6ec3f0dbca72ecb87ec
Ilya Bashlyaev <ibashlyaev2000@gmail.com>
2022-10-30 21:40:23 +0100

    Commit

--- db.php
+++ db.php
@@ -1,8 +1,7 @@
 <?php
     if ($_SERVER['HTTP_HOST'] == 'fresh-and-tasty.herokuapp.com')
-        $connection = new mysqli('sql11.freemysqlhosting.net', 'sql11511502', '9PXMcq2Mnj', 'sql11511502');
+        $connection = new mysqli('sql7.freesqldatabase.com', 'sql7530863', 'tlEaSEVMyG', 'sql7530863');
 
     else
         $connection = new mysqli('127.0.0.1', 'Ilya Bashlyaev', '#vOV(0y2#vOV(0y2', 'restaurant');
-
 ?>
